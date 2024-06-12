5191d006b255bbeb0e608afbff8877c1d2e82d5e
saadman.sayeed <saadman.sayeed@bjitgroup.com>
2024-06-12 10:35:12 +0600

    removed env

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlice';
 import logger from 'redux-logger';
+import Env from '../lib/Env';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
   },
+  middleware:[
+
+  ],
+  devTools:Env.NODE_ENV !== 'production'
 //   middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(logger)
 });
 
